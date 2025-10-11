44081ab8cb01359b58ee608351381ca1d7b92b83
Eelco van Vliet <eelcovv@gmail.com>
2025-10-11 11:37:24 +0200

    small fix to prevent future wanring

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 
 ROOT = Path(__file__).resolve().parent.parent  # repo root
-TEST_DATA = ROOT / "tests" / "data"
+TEST_DATA = ROOT / "tests" / "files"
 
 
 @pytest.fixture(scope="session")
